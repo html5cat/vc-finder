21cae9872163b9fb977f26b8a59937f8a6ba7fb0
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-04-21 16:38:28 -0700

    Add sorting and data note

--- src/App.js
+++ src/App.js
@@ -37,14 +37,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Welcome to VC Finder</h2>
+          <p>Data by <a href="https://twitter.com/morganpolotan">Morgan Polotan</a></p>
+          <p><a onClick={ this.handlerClickCleanFiltered.bind(this) } style={ { cursor: 'pointer' } }>clear filters</a></p>
         </div>
         <BootstrapTable ref='table' data={ Array.isArray(this.state.data)? this.state.data : [] }>
-          <TableHeaderColumn dataField='Investor Name' isKey>
-            Investor Name
-            <br/><a onClick={ this.handlerClickCleanFiltered.bind(this) } style={ { cursor: 'pointer' } }>clear filters</a>
-          </TableHeaderColumn>
-          <TableHeaderColumn ref='firm' dataField='Firm' filter={ { type: 'TextFilter', placeholder: 'Please enter a value' } }>Firm</TableHeaderColumn>
-          <TableHeaderColumn ref='stage' dataField='Company Stage (check all)' filter={ { type: 'TextFilter', placeholder: 'Please enter a value' } }>Firm</TableHeaderColumn>
+          <TableHeaderColumn dataField='Investor Name' dataSort={ true } isKey>Investor Name</TableHeaderColumn>
+          <TableHeaderColumn ref='firm' dataField='Firm' dataSort={ true } filter={ { type: 'TextFilter', placeholder: 'Please enter a value' } }>Firm</TableHeaderColumn>
+          <TableHeaderColumn ref='stage' dataField='Company Stage (check all)' dataSort={ true } filter={ { type: 'TextFilter', placeholder: 'Please enter a value' } }>Company Stage</TableHeaderColumn>
 
         </BootstrapTable>
       </div>

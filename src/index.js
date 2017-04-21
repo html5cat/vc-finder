2af065d69a515b454e8efa89821927c0e46a31f9
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-04-21 15:37:25 -0700

    Add bootstrap and it's kinda working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 ReactDOM.render(
   <App />,
   document.getElementById('root')
